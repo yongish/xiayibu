e3aef2bf255050e745758dee375ec293042211d0
Zhi Yong Tan <yongish@gmail.com>
2018-12-06 15:37:57 -0800

    Room Database. Need to remove fallbackToDestructiveMigration.

---
+++ app/src/main/java/com/zhiyong/xiayibu/WordRoomDatabase.java
@@ -0,0 +1,27 @@
+package com.zhiyong.xiayibu;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
