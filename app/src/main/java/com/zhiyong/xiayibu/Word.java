d1431f33c0eb288b80ab863f0141540e0689bc14
Zhi Yong Tan <yongish@gmail.com>
2018-12-06 00:01:00 -0800

    Word entity.

---
+++ app/src/main/java/com/zhiyong/xiayibu/Word.java
@@ -0,0 +1,29 @@
+package com.zhiyong.xiayibu;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity
+public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    private long timestamp;
+
+    public Word(@NonNull String mWord, @NonNull long timestamp) {
+        this.mWord = mWord;
+        this.timestamp = timestamp;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
