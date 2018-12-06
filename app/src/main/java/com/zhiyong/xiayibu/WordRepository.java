1ee265e802f3caa1c673f271dc241680a5ad40d9
Zhi Yong Tan <yongish@gmail.com>
2018-12-06 15:56:51 -0800

    repository

---
+++ app/src/main/java/com/zhiyong/xiayibu/WordRepository.java
@@ -0,0 +1,40 @@
+package com.zhiyong.xiayibu;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    public static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+            mAsyncTaskDao.insert(words[0]);
+            return null;
+        }
+    }
+}
