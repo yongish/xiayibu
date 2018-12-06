aa071107744aad81f3b77e259f226d35438a75b4
Zhi Yong Tan <yongish@gmail.com>
2018-12-06 15:31:59 -0800

    livedata

--- app/src/main/java/com/zhiyong/xiayibu/WordDao.java
+++ app/src/main/java/com/zhiyong/xiayibu/WordDao.java
@@ -1,5 +1,6 @@
 package com.zhiyong.xiayibu;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -18,5 +19,5 @@ public interface WordDao {
     void delete(String wordString);
 
     @Query("SELECT * from word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 }
